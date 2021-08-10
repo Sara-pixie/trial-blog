cc8e0b3d7238198e06b4bec9c6e0a86775a3f0e6
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-08-10 19:53:15 +0200

    View Engine (ejs) templates created with partials
    tutorial link: https://www.youtube.com/watch?v=yXEesONd_54&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=8

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ const express = require('express');
 //express app
 const app = express();
 
+//set view engine
+app.set('view engine', 'ejs'); //will automatically look into views folder
+
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        {title: 'Title 1', snippet: 'This is a smal snippet #1'},
+        {title: 'Title 2', snippet: 'This is a smal snippet #2'},
+        {title: 'Title 3', snippet: 'This is a smal snippet #3'},
+    ];
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
 });
 
-//redirect option
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'New Blog' });
 });
 
 // default 404 page 
 app.use((req, res) => { //fires for every request made, but only if the request reaches this point!
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404' });
 });
