ceb778401803fa93da585282944720e216301bc9
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-08-10 09:42:08 +0200

    Server responds
    Tutorial link (up to 9 min): https://www.youtube.com/watch?v=DQD00NAUPNk&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=4

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const http = require('http');
 
 //When request is made
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello, World!</h1>');
+    res.write('<p>This is how we get started</p>');
+    res.end();
 });
 
 //server is listening for requests
